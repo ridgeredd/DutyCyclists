a9ac1ccc8838bb8f053dc08939017eb65146b67e
Ridge R <113300015+ridgeredd@users.noreply.github.com>
2025-10-20 16:27:13 -0400

    switched to sending bytes. added Reed Solomon capability

--- esp32/main/main.c
+++ esp32/main/main.c
@@ -22,6 +22,8 @@
 #include "driver/gpio.h"
 #include "rom/ets_sys.h"
 
+#include "libfec/fec.h"
+
 static const char *TAG = "gps_tx";
 
 #define SAMPLE_RATE             48000                   // 48 kHz sample rate; ideally this is a multiple of each symbol frequency
@@ -41,21 +43,17 @@ static const float SYMBOL_FREQS[NSYMBOLS]   = { 1200.0f, 2200.0f }; // symbol fr
 #define CODEWORD_BITS           4           // how many bits are combined for RS code alphabet
 #define REDUNDANT_BITS          4           // how many bits are appended for error correction
 
-#define PAYLOAD_LEN             48
-#define TRANSMISSION_LEN        64          //(2 * 8) + PAYLOAD_LEN + REDUNDANT_BITS        // Total bits being sent
-
-#define TRANSMISSION_TIME_MS    (1000 * SAMPLES_PER_SYMBOL * TRANSMISSION_LEN) / (BITS_PER_SYMBOL * SAMPLE_RATE)
-#define MIN_INTERVAL_MS         2 * TRANSMISSION_TIME_MS   // Don't allow transmissions within 2 * min interval of eachother 
-
-#define AUTOMATIC_TRANSMISSION       1           // 1 enables 0 disables auto transmission at regular interval
+#define AUTOMATIC_TRANSMISSION       0           // 1 enables 0 disables auto transmission at regular interval
 #define TIMER_DELAY_MS               10000       // milliseconds before resending signal
 static volatile uint64_t last_tx_us = 0;         // Last transmission time
 
 #define PTT_INPUT_PIN                 GPIO_NUM_4
 #define PTT_OUTPUT_PIN                GPIO_NUM_5
 #define PTT_BUFFER_MS                 10        // amount of additional time you hold PTT high after transmission length
-#define PTT_ACTIVE_LVL                1         // active high (1) or low (0)
-#define PTT_INACTIVE_LVL              !PTT_ACTIVE_LVL
+#define PTT_ACTIVE_LVL                0         // active high (1) or low (0)
+#define PTT_INACTIVE_LVL              1
+
+#define CALL_SIGN                     0x69      // unique id for this device
 
 #if PTT_ACTIVE_LVL
     #define PTT_PRESS_INTR            EDGE_RISING // Press is rising edge     
@@ -66,6 +64,17 @@ static volatile uint64_t last_tx_us = 0;         // Last transmission time
 #endif         
 
 #define DAC_AMPLITUDE_V                1.0    // Must be within [0, 3.3]
+#define START_STOP                     0x7E   // starting and stopping condition
+
+#define PARITY_BYTES                   6      // These must sum to < 255
+#define PAYLOAD_BYTES                  10      
+    
+# define TRANSMISSION_BYTES             PAYLOAD_BYTES + PARITY_BYTES + 3
+
+#define TRANSMISSION_TIME_MS    (1000 * SAMPLES_PER_SYMBOL * 8 * TRANSMISSION_BYTES) / (BITS_PER_SYMBOL * SAMPLE_RATE)
+#define MIN_INTERVAL_MS         2 * TRANSMISSION_TIME_MS   // Don't allow transmissions within 2 * min interval of eachother 
+#define BAUD_RATE               SAMPLE_RATE / SAMPLES_PER_SYMBOL
+#define BIT_RATE                BAUD_RATE * BITS_PER_SYMBOL
 
 #if AUTOMATIC_TRANSMISSION
 static TimerHandle_t auto_gps_timer = NULL;         // automatically requests a gps send at regular intervals
@@ -73,14 +82,17 @@ static TimerHandle_t auto_gps_timer = NULL;         // automatically requests a 
 static TimerHandle_t ptt_disable_timer = NULL;      // times how long to keep ptt high for  
 static TaskHandle_t send_gps_handle = NULL;         // task handle to request transmission
 
-// Send a string of 32 bit words; Sends symbols in little-endian
-// TODO: implement cosine look up table; Separate this into a different file
-static void encode_words(uint32_t *words, uint32_t nwords, int16_t *buf) {
+static void* rs_encoder = NULL;
 
-    int word_idx = 0;
+// Send a string of bytes; Sends symbols in little-endian
+// TODO: implement cosine look up table; Separate this into a different file
+// TODO: issue. takes about twice as long
+static void tx_bytes(uint8_t *bytes, uint32_t nbytes, int16_t *buf){
+
+    int byte_idx = 0;
     int bit_idx = BITS_PER_SYMBOL;
-    uint32_t word = words[0];
-    float freq = SYMBOL_FREQS[word & SYMBOL_MASK];
+    uint32_t byte = bytes[0];
+    float freq = SYMBOL_FREQS[byte & SYMBOL_MASK];
 
     static float phase = 0;
     static int count = 0;   // times the symbol switches
@@ -101,21 +113,21 @@ static void encode_words(uint32_t *words, uint32_t nwords, int16_t *buf) {
             if (i == next_switch) {
 
                 // increment bits
-                if (bit_idx < 32) {
-                    word = word >> BITS_PER_SYMBOL;
-                    freq = SYMBOL_FREQS[word & SYMBOL_MASK];
+                if (bit_idx < 8) {
+                    byte = byte >> BITS_PER_SYMBOL;
+                    freq = SYMBOL_FREQS[byte & SYMBOL_MASK];
                     bit_idx += BITS_PER_SYMBOL;
                 } 
                 // increment to new word
                 else {
-                    if (word_idx == nwords - 1) { // transmission finished
+                    if (byte_idx == nbytes - 1) { // transmission finished; auto flush buffer
                         i2s_write(I2S_NUM_0, buf, 2 * (i + 1) * sizeof(int16_t), &written, portMAX_DELAY); 
                         return;
                     }
                     else {
-                        word_idx++;
-                        word = words[word_idx];
-                        freq = SYMBOL_FREQS[word & SYMBOL_MASK];
+                        byte_idx++;
+                        byte = bytes[byte_idx];
+                        freq = SYMBOL_FREQS[byte & SYMBOL_MASK];
                         bit_idx = BITS_PER_SYMBOL;
                     }
                 }
@@ -127,40 +139,69 @@ static void encode_words(uint32_t *words, uint32_t nwords, int16_t *buf) {
             if (phase >= 2 * PI) { phase -= 2 * PI; }
 
         }
+        // buffer full; flush
         i2s_write(I2S_NUM_0, buf, 2 * BUF_LEN * sizeof(int16_t), &written, portMAX_DELAY);
         vTaskDelay(pdMS_TO_TICKS(1)); // delay for some amount of time to free up other tasks
         count += BUF_LEN;
-        if (count > SAMPLES_PER_SYMBOL) { count -= SAMPLES_PER_SYMBOL; } // wrap around if transmit changed
+        if (count > SAMPLES_PER_SYMBOL) { count -= SAMPLES_PER_SYMBOL; } // wrap around if transmitted symbol changed
         // it is important this is > not >= bc >= would cause issue where new count == samples per symbol, would reset to 0 rather than changing in next iteration
 
     }
 
 }
 
-// TODO: add start stop conditions and error correction
-static void send_gps() {
+// TODO: implement
+// currently stub
+static void get_gps(uint8_t *bytes) {
+    for (uint8_t count = 0; count < PAYLOAD_BYTES; count++) {
+        bytes[count] = count;
+    }
+}
+
+// Continuously running process that wakes to send gps transmission
+static void tx_gps_task() {
 
     int16_t *buf = (int16_t*)heap_caps_malloc(BUF_LEN * 2 * sizeof(int16_t), MALLOC_CAP_DEFAULT);
+    uint8_t *tx_data = (uint8_t*)heap_caps_malloc(TRANSMISSION_BYTES * sizeof(uint8_t), MALLOC_CAP_DEFAULT);
+    tx_data[0] = START_STOP;
+    tx_data[1] = CALL_SIGN;
+    tx_data[TRANSMISSION_BYTES - 1] = START_STOP;
 
     while(1) {
 
-        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-        uint32_t words[] = { 0xA4AB9AA0, 0xA4083A5A };   // literal is temporary
-        uint32_t nwords = 2;
-        gpio_set_level(PTT_OUTPUT_PIN, PTT_ACTIVE_LVL);
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);                // wait until woken up
+        uint8_t *payload = (uint8_t*)(tx_data + 2);             // pointer to the payload bytes
+        get_gps(payload);                                       // currently stubbed
+        uint8_t *parity = (uint8_t*)(payload + PAYLOAD_BYTES);  // pointer to the parity bytes
+
+        printf("Data: ");
+        for (size_t i = 0; i < PAYLOAD_BYTES; i++) {
+            printf("%02X ", payload[i]);  // two-digit uppercase hex
+        }
+        printf("\n");
+
+        encode_rs_char(rs_encoder, payload, parity); // set parity bytes
+
+        printf("Encoded: ");
+        for (size_t i = 0; i < TRANSMISSION_BYTES; i++) {
+            printf("%02X ", tx_data[i]);  // two-digit uppercase hex
+        }
+        printf("\n\n");
+
+        gpio_set_level(PTT_OUTPUT_PIN, PTT_ACTIVE_LVL); // set ptt active
         xTimerStart(ptt_disable_timer, 0);  // start timer to reset ptt after some time
-        encode_words(words, nwords, buf);
+        tx_bytes(tx_data, TRANSMISSION_BYTES, buf);
 
     }
 
 }
 
-#if AUTOMATIC_TRANSMISSION
+
 // Resets ptt after gps transmission
 static void disable_ptt_timer_callback() {
     gpio_set_level(PTT_OUTPUT_PIN, PTT_INACTIVE_LVL);
 }
-#endif
+
 
 // Spawns send gps task when ptt releases, or drives ptt active when ptt pushed
 static void IRAM_ATTR ptt_interrupt_handler() { 
@@ -235,18 +276,18 @@ static void config_dac() {
 static void config_ptt() {
 
     gpio_config_t ptt_in_conf = {
-        .intr_type = GPIO_INTR_ANYEDGE,  // activates on either edge
+        .intr_type = GPIO_INTR_POSEDGE,  // activates on rising edge
         .mode = GPIO_MODE_INPUT,
         .pin_bit_mask = (1ULL << PTT_INPUT_PIN),
-        .pull_up_en = GPIO_PULLUP_ENABLE,   // enable if signal is open drain
+        .pull_up_en = GPIO_PULLUP_ENABLE,   // enable for PTT
         .pull_down_en = GPIO_PULLDOWN_DISABLE
     };
     gpio_config(&ptt_in_conf);
 
     gpio_config_t ptt_out_conf = {
         .pin_bit_mask = 1ULL << PTT_OUTPUT_PIN,
-        .mode = GPIO_MODE_OUTPUT,
-        .pull_up_en = GPIO_PULLUP_DISABLE,     // pulls not needed for push-pull output
+        .mode = GPIO_MODE_OUTPUT_OD,                  // Open drain. "Pull Down"
+        .pull_up_en = GPIO_PULLUP_DISABLE,     // Do not pull up or down for output
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
         .intr_type = GPIO_INTR_DISABLE
     };
@@ -306,21 +347,30 @@ static void NRZI(uint32_t *words, uint32_t nwords) {
 
 }
 
-static void envelope() {
-
-    
-
-}
-
-static void reed_solomon() {
-
-
-
-}
-
 void app_main(void) {
 
-    xTaskCreate(send_gps, "gps_task", 4096, NULL, 3 /*priority*/, &send_gps_handle);
+    printf("Transmission Time: %d ms\n", TRANSMISSION_TIME_MS);
+    printf("Baud Rate: %d sym/sec\n", BAUD_RATE);
+    printf("Bit Rate: %d bit/sec\n", BIT_RATE);
+    #if PTT_ACTIVE_LVL
+        printf("PTT Active High\n");
+    #else
+        printf("PTT Active Low\n");
+    #endif
+
+    // Initialize reed-solomon encoder. Requirements for library specified here: 
+    // https://manpages.debian.org/unstable/libfec-dev/rs.3.en.html?utm_source=chatgpt.com
+    rs_encoder = init_rs_char(
+        8, // gives the symbol size in bits, up to 32
+        0x11D, // gives the extended Galois field generator polynomial coefficients, with the 0th coefficient in the low order bit. The polynomial must be primitive; if not, the call will fail and NULL will be returned.
+		0, // gives, in index form, the first consecutive root of the Reed Solomon code generator polynomial
+        1, // gives, in index form, the primitive element in the Galois field used to generate the Reed Solomon code generator polynomial.
+        PARITY_BYTES, // gives the number of roots in the Reed Solomon code generator polynomial. This equals the number of parity symbols per code block.
+		255 - PARITY_BYTES - PAYLOAD_BYTES // gives the number of leading symbols in the codeword that are implicitly padded to zero in a shortened code block.
+        // The resulting Reed-Solomon code has parameters (N,K), where N = 2^symsize - pad - 1 and K = N-nroots.
+    );
+
+    xTaskCreate(tx_gps_task, "gps_task", 4096, NULL, 3 /*priority*/, &send_gps_handle);
 
     #if MIN_INTERVAL_MS < TRANSMISSION_TIME_MS
         ESP_LOGI(TAG, "Ensure that minimum interval time between transmission is greater than transmission time");
